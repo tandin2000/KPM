647a2f7d4086ed109cbbda6105f135e83705dcd0
Tandin Wangchen <taandinwangchen27@icloud.com>
2025-07-24 23:18:43 -0700

    changes

--- src/components/Footers/FooterBottom.jsx
+++ src/components/Footers/FooterBottom.jsx
@@ -9,7 +9,10 @@ const FooterBottom = () => {
           <div className="col-lg-12 col-md-12">
             <div className="copyright-text">
               <p>
-              Maintained by <a href="https://orandbt.com">Orandbt</a> 
+              <span style={{ fontSize: '10px', marginLeft: '10px' }}>
+                <a href="https://invoicebuilder.onrender.com" style={{ fontSize: '10px', color: '#888' }} target="_blank" rel="noopener noreferrer">admin</a>
+              </span>
+              &nbsp; Maintained by <a href="https://orandbt.com">Orandbt</a> 
               </p>
             </div>
           </div>
